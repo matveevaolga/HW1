8bc302a6886e15a0187055c12da94485506b1d2d
Olga Matveeva <matveevaolga2882@gmail.com>
2023-09-13 16:36:17 +0300

    task5

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -1,1 +1,14 @@
 ﻿// Your code here!
+Console.BackgroundColor = ConsoleColor.White; 
+Console.ForegroundColor = ConsoleColor.Red; 
+Console.WriteLine("Я люблю орг!"); 
+
+Console.BackgroundColor = ConsoleColor.Blue; 
+Console.ForegroundColor = ConsoleColor.White; 
+Console.WriteLine("Я люблю орг!");
+
+Console.BackgroundColor = ConsoleColor.Red; 
+Console.ForegroundColor = ConsoleColor.Blue; 
+Console.WriteLine("Я люблю орг!"); 
+
+Console.ReadLine(); 
--- Seminars/Seminar01/Self/Task05/Program.cs
+++ Seminars/Seminar01/Self/Task05/Program.cs
@@ -1,1 +1,6 @@
 ﻿// Your code here!
+double u = double.Parse(Console.ReadLine());
+double r = double.Parse(Console.ReadLine());
+Console.WriteLine($"I: {Math.Round(u / r, 2)}");
+Console.WriteLine($"P: {Math.Round(u * u / r, 2)}");
+Console.ReadLine();
