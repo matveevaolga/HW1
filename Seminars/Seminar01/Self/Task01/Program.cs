83583ee666d605618ff37705ead7d534f83f22e2
Olga Matveeva <matveevaolga2882@gmail.com>
2023-09-13 12:15:55 +0300

    task1

--- Seminars/Seminar01/Self/Task01/Program.cs
+++ Seminars/Seminar01/Self/Task01/Program.cs
@@ -1,1 +1,7 @@
 ﻿// Your code here!
+string surname = Console.ReadLine();
+string name = Console.ReadLine();
+string middlename = Console.ReadLine();
+
+Console.WriteLine("Фамилия: {0}\nИмя: {1}\nОтчество: {2}", surname, name, middlename);
+Console.ReadLine();
