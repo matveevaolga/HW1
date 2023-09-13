91491ad7ad069256b5048c8e8b5d65a01d809821
Olga Matveeva <matveevaolga2882@gmail.com>
2023-09-13 15:20:46 +0300

    task4

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -1,1 +1,14 @@
 ﻿// Your code here!
+Console.BackgroundColor = ConsoleColor.White; // устанавливаем белый цвет фона для текста
+Console.ForegroundColor = ConsoleColor.Red; // устанавливаем красный цвет шрифта
+Console.WriteLine("Я люблю орг!"); // выводим строку
+
+Console.BackgroundColor = ConsoleColor.Blue; // устанавливаем белый цвет фона для текста
+Console.ForegroundColor = ConsoleColor.White; // устанавливаем красный цвет шрифта
+Console.WriteLine("Я люблю орг!"); // выводим строку
+
+Console.BackgroundColor = ConsoleColor.Red; // устанавливаем белый цвет фона для текста
+Console.ForegroundColor = ConsoleColor.Blue; // устанавливаем красный цвет шрифта
+Console.WriteLine("Я люблю орг!"); // выводим строку
+
+Console.ReadLine(); // ожидание нажатия любой кнопки
